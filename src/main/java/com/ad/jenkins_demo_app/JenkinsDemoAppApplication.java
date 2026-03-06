39d32cd8db44f14e13965ba5251ed26745b7c6f2
Adith KV <adithkv@Adiths-MacBook-Air.local>
2026-03-06 23:20:18 +0530

    code pushed

---
+++ src/main/java/com/ad/jenkins_demo_app/JenkinsDemoAppApplication.java
@@ -0,0 +1,21 @@
+package com.ad.jenkins_demo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsDemoAppApplication {
+
+	@GetMapping("/jenkinstest/{name}")
+	public String jenkinsTest(@PathVariable String name){
+		return "Hello" + name + "Congrats you have successfully completed jenkins demo";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ad/jenkins_demo_app/JenkinsDemoAppApplicationTests.java
@@ -0,0 +1,31 @@
+package com.ad.jenkins_demo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+class JenkinsDemoAppApplicationTests {
+
+	@Autowired
+	private JenkinsDemoAppApplication application;
+
+	@Test
+	void contextLoads() throws Exception {
+		String name = "Basanta kumar hota";
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(application).build();
+
+		mockMvc.perform(get("/jenkinstest/{name}", name))
+					.andExpect(status().isOk())
+					.andExpect(content().string(
+							"Hello" + name + "Congrats you have successfully completed jenkins demo"
+					));echo "# jenkins-ci-cd" >> README.md
+	}
+
+}
